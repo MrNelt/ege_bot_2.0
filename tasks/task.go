13ff0126e70a680a0b77a6510a9492a905b76778
Markit125 <72036187+Markit125@users.noreply.github.com>
2023-04-09 19:57:00 +0300

    tasks choice

---
+++ tasks/task.go
@@ -0,0 +1,75 @@
+package tasks
+
+import (
+	"math/rand"
+	"time"
+)
+
+func randInt(min, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+func getThreeNumbers(min, max int) [3]int {
+	set := make(map[int]bool)
+
+	for len(set) != 3 {
+		set[randInt(min, max)] = true
+	}
+
+	arr := [3]int{}
+
+	i := 0
+	for num := range set {
+		arr[i] = num
+		i++
+	}
+
+	return arr
+}
+
+func GetTask(blackWords []string, words []string) ([4]string, int) {
+
+	rand.Seed(time.Now().UnixNano())
+
+	var task [4]string
+
+	task[0] = words[randInt(0, len(words))]
+	answer := task[0]
+
+	numbers := getThreeNumbers(0, len(blackWords))
+
+	for i := 0; i < 3; i++ {
+		task[i+1] = blackWords[numbers[i]]
+	}
+
+	rand.Shuffle(len(task), func(i, j int) {
+		task[i], task[j] = task[j], task[i]
+	})
+
+	i := 0
+	for task[i] != answer {
+		i++
+	}
+
+	return task, i + 1
+}
+
+// func main() {
+
+// 	content, err := ioutil.ReadFile("blackWords.txt")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	blackWords := strings.Split(string(content), "\n")
+
+// 	content, err = ioutil.ReadFile("words.txt")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	words := strings.Split(string(content), "\n")
+
+// 	task, answer := GetTask(blackWords, words)
+
+// 	fmt.Println(task, answer)
+
+// }
