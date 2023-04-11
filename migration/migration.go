b56e6e3be1bc669921234aec20cf121743ffb3ee
kappaprideonly <kolmanoney@gmail.com>
2023-04-12 01:50:34 +0300

    error exist

--- migration/migration.go
+++ migration/migration.go
@@ -1,16 +1,21 @@
 package main
 
 import (
-	"github.com/kappaprideonly/ege_bot_2.0/config"
+	"log"
+
+	"github.com/joho/godotenv"
 	"github.com/kappaprideonly/ege_bot_2.0/database"
 	"github.com/kappaprideonly/ege_bot_2.0/model"
 )
 
 func init() {
-	config.Init()
+	if err := godotenv.Load("../config/.env"); err != nil {
+		log.Panic("No .env file found")
+	}
 }
 
 func main() {
 	db := database.GetDB()
 	db.AutoMigrate(&model.User{})
+	db.Delete(&model.User{}, "name LIKE ?", "Markit")
 }
